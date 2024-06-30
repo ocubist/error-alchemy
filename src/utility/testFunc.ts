e9a828e04fea7a2957fd43f862022b6b52270bc8
ocubist <stebenbla@gmail.com>
2024-06-30 00:33:33 -0500

    add auto-pull-request

--- src/utility/testFunc.ts
+++ src/utility/testFunc.ts
@@ -1,7 +1,6 @@
+/**
+ * JS DOCS added
+ */
 export const testFunc = () => {
-  console.log("works!!!!!!");
-};
-
-export const testFunc2 = () => {
-  console.log("works!@!!!!!!!");
+  console.log("works!");
 };
